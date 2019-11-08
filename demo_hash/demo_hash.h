389046202cff2865dfe0789bb004188d1fad1616
龙杜平 <longduping@qianxin.com>
2019-11-08 10:14:15 +0800

    ADD 1.操作系统环境变量 2.跨函数跳转 3.alloca 栈空间分配 4.C程序的内存区域

---
+++ demo_hash/demo_hash.h
@@ -0,0 +1,136 @@
+//
+// Created by longduping on 2019/11/7.
+//
+
+#ifndef C_LEARNING_DEMO_HASH_H
+#define C_LEARNING_DEMO_HASH_H
+
+#include <elf.h>
+#include <ctype.h>
+
+
+/* Thomas Wang's 32 bit Mix Function
+ *
+ * 功能：计算int类型的hash，返回值也是int类型。
+ * 特点：高性能（避免了乘法和除法运算）
+ *
+ * */
+unsigned int IntHash(unsigned int key)
+{
+    key += ~(key << 15U);
+    key ^= (key >> 10U);
+    key += (key << 3U);
+    key ^= (key >> 6U);
+    key += ~(key << 11U);
+    key ^= (key >> 16U);
+    return key;
+}
+
+static uint32_t hash_seed = 5381;
+
+/* MurmurHash2, by Austin Appleby
+ * Note - This code makes a few assumptions about how your machine behaves -
+ * 1. We can read a 4-byte value from any address without crashing
+ * 2. sizeof(int) == 4
+ *
+ * And it has a few limitations -
+ *
+ * 1. It will not work incrementally.
+ * 2. It will not produce the same results on little-endian and big-endian
+ *    machines.
+ *
+ * 特点：高性能，低碰撞
+ */
+unsigned int MurmurHash2(const void *key, size_t len)
+{
+    /* 'm' and 'r' are mixing constants generated offline.
+     They're not really 'magic', they just happen to work well.  */
+    uint32_t seed = hash_seed;
+    const uint32_t m = 0x5bd1e995;
+    const int r = 24;
+
+    /* Initialize the hash to a 'random' value */
+    uint32_t h = seed ^len;
+
+    /* Mix 4 bytes at a time into the hash */
+    const unsigned char *data = (const unsigned char *) key;
+
+    while (len >= 4) {
+        uint32_t k = *(uint32_t *) data;
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h *= m;
+        h ^= k;
+
+        data += 4;
+        len -= 4;
+    }
+
+    /* Handle the last few bytes of the input array  */
+    switch (len) {
+        case 3:
+            h ^= data[2] << 16;
+        case 2:
+            h ^= data[1] << 8;
+        case 1:
+            h ^= data[0];
+            h *= m;
+    };
+
+    /* Do a few final mixes of the hash to ensure the last few
+     * bytes are well-incorporated. */
+    h ^= h >> 13;
+    h *= m;
+    h ^= h >> 15;
+
+    return (unsigned int) h;
+}
+
+/* DJB Hash
+ *
+ */
+unsigned int DJBHash(char *str)
+{
+    unsigned int hash = (unsigned int) hash_seed;
+
+    while (*str) {
+        hash = ((hash << 5) + hash) + (*str++); /* times 33 */
+    }
+    hash &= ~(1 << 31); /* strip the highest bit */
+    return hash;
+}
+
+/* DJB Case Hash(不区分大小写)
+ *
+ */
+unsigned int DJBCaseHash(const unsigned char *buf, size_t len)
+{
+    unsigned int hash = (unsigned int) hash_seed;
+
+    while (len--)
+        hash = ((hash << 5) + hash) + (tolower(*buf++)); /* hash * 33 + c */
+    return hash;
+}
+
+
+/* 32位FNV算法(乘法)
+ * 核心思想：将16777619进制转为10进制。
+ */
+unsigned int M_SHIFT = 0;
+unsigned int M_MASK = 0x8765fed1;
+
+unsigned int FNVHash(const unsigned char *key, size_t len)
+{
+    unsigned int hash = (int) 2166136261L;
+    while (len--)
+        hash = (hash * 16777619) ^ *(key + len);
+
+    if (M_SHIFT == 0)
+        return hash;
+    return (hash ^ (hash >> M_SHIFT)) & M_MASK;
+}
+
+#endif //C_LEARNING_DEMO_HASH_H
--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <setjmp.h>
 
+/*                C程序 内存区域
+ * +----------------------------------------+
+ * |               env and argv             |       +-----------+ ——> %ebp (基地址指针)
+ * +----------------------------------------+       |           |
+ * |                                        |       |  函数栈帧  |
+ * |                栈空间 ↓                 |       |           |
+ * |                                        |       +------------ ——> %esp (栈指针)
+ * +------------共享库和内存映射区------------+
+ * |                                        |
+ * |                堆空间 ↑                 |
+ * |                                        |
+ * +----------------------------------------+
+ * |            未初始化数据区                |
+ * +----------------------------------------+
+ * |            初始化数据区（自动变量）       |
+ * +----------------------------------------+
+ * |            程序正文（代码段）            |
+ * +----------------------------------------+
+ */
 void demo_bool()
 {
     // C语言中的布尔类型, 任何非0值都会被转为1
@@ -24,7 +45,7 @@ void demo_rand()
 
 void demo_malloc()
 {
-    // malloc
+    /* malloc */
     void *p = malloc(1); // 分配1 byte大小的内存空间，将内存地址赋值给c
     // 由于malloc返回的是通用指针类型 void，这里需要转换成实际的指针类型
     *((char *) p) = 'A';
@@ -34,14 +55,70 @@ void demo_malloc()
     *((char *) p) = 'B'; // 释放内存后仍然可以对该内存区域操作，但是不安全，随时会被覆盖
     printf("c的地址：%p , c的值:%c \n", p, *((char *) p));
 
-    // calloc ，与malloc的区别: calloc分配完成后会将内存块中所有的位 置为：0
+    /* calloc  */
+    // 与malloc的区别: calloc分配完成后会将内存块中所有的位 置为：0
     void *ptr = calloc(2, 1);// 申请分配2个单元，其中每个单元4字节的内存空间（共2 * 4 = 8 字节）。
     *((char *) ptr) = 'a';
-    *((char *) ptr + 1) = 'b'; // 加1表示 偏移一个单位大小的字节(该单位取决于被转换的指针类型 所执向的数据结构大小，int=4, char=1...)
+    *((char *) ptr + 1) = 'b'; // 加1表示 偏移一个单位大小的字节(该单位大小取决于被转换的指针 所执向的数据结构大小，int=4, char=1...)
     printf("ptr的地址：%p , ptr的值:%d , %d \n", ptr, *((char *) ptr), *((char *) ptr + 1));
     free(ptr);
+
+    /* realloc 
+     *
+     * 增加或减少之前分配区的长度（可能或将原有数据整体移动到另一个地方，新增部分的初始值则不确定）
+     * */
+    void *p_old = malloc(1);
+    printf("old=%s\n", p_old);
+    // 重新分配两个字节空间,当第一个参数为NULL时，realloc的行为与malloc一致
+    void *p_new = realloc(p_old, 2);
+    printf("new=%s\n", p_new);
+    free(p_new);
+
+    /* alloca
+     *
+     * 在栈上分配空间，（程序调用结束后会自动释放所分配的空间）。需要注意的是：某些系统不支持程序被加载后动态调整栈大小。
+     * */
+    void *pstack = alloca(1024);
+    printf("ptr (stack): %p\n", pstack);
 }
 
+void demo_sysenv()
+{
+    /* 获取系统的环境变量 */
+    char *value = getenv("PATH");
+    printf("PATH=%s\n", value);
+
+    /* 设置系统环境变量 */
+    int res = putenv("DEMO_ENV=it's work!");
+    printf("putenv   status: %d ,DEMO_ENV=%s\n", res, getenv("DEMO_ENV"));
+
+    // 设置系统环境变量，第三个参数: 非0时若该变量存在，则先删除已有定义再设置。为0时若该变量存在，则什么也不做。
+    res = setenv("DEMO_ENV", "yes, it's work!", 1);
+    printf("setenv   status: %d ,DEMO_ENV=%s\n", res, getenv("DEMO_ENV"));
+
+    // 删除系统环境变量
+    res = unsetenv("DEMO_ENV");
+    printf("unsetenv status: %d ,DEMO_ENV=%s\n", res, getenv("DEMO_ENV"));
+}
+
+jmp_buf jmp_buffer;
+
+void demo_jmp()
+{
+    /* 提供跨函数的goto功能 */
+    printf("enter demo_jmp()\n");
+    /* 非局部跳转： 调用longjmp后，程序直接返回到 jmp_buffer中记录的栈帧处。 */
+    //longjmp(jmp_buffer, -1);
+}
+
+void demo_limit()
+{
+    /* 查询和设置系统资源限制
+     * getrlimit()
+     * setrlimit()       */
+}
+
+/*---------------------------------------------------------------------------*/
 
 struct command {
     char *name;
@@ -54,14 +131,30 @@ struct command cmds[] = {
         {"demo_bool",   1, demo_bool},
         {"demo_rand",   1, demo_rand},
         {"demo_malloc", 1, demo_malloc},
+        {"demo_sysenv", 1, demo_sysenv},
+        {"demo_jmp",    1, demo_jmp},
+        {"demo_limit",  1, demo_limit},
         /* 命令模式 add more... */
         {NULL,          0, NULL},
 };
 
+
 int main()
 {
     int i = 0;
-
+    /* 记录该栈帧的一些信息（包括此处的返回地址），若返回值为0，则为首次调用。若非0，则是从别处跳过来的。 */
+    int jmpno = setjmp(jmp_buffer);
+    switch (jmpno) {
+        case 0:
+            // 首次调用，记录此时的栈信息，并保存在全局变量jmp_buffer中。
+            break;
+        case -1:
+            printf("demo_jmp() error!");
+            return 1;
+        default:
+            printf("other error! %d\n", jmpno);
+            break;
+    }
     while (cmds[i].func) {
         if (!cmds[i].enable) continue;
         printf("==========%s()============ \n", cmds[i].name);
